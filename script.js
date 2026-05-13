642ef7eec27ab5908e45c15d2a1a11f47ad541c3
Praktikant Slot <praktikant.slot@v21.fazi.rs>
2026-05-13 17:31:45 +0200

    Zavrsen korak 4.

--- script.js
+++ script.js
@@ -1,35 +1,106 @@
-const cards = [
+const cards = [ 
     "😘", 
-    "❤️",
-    "👯‍♀️",
+    "❤️", 
+    "👯‍♀️", 
     "👩🏼‍💻",
     "🌹",
-    "😻",
+    "😻", 
     "🪄",
     "🦄"];
 
 //dupliranjee spread operator
 const gameCard=[...cards, ...cards];
 
-//shuffle-mesanje
+//mesanje karata shuffle
 gameCard.sort (()=> Math.random() - 0.5);
 
-
-//
+//uzimamoo board 
 const gameBoard = document.getElementById("gameBoard");
 
-//ovde kartica
+let firstCard = null;
+let secondCard = null; 
+let lockBoard = false;
+
+//ovde kartice pravimo
 gameCard.forEach(emoji => {
 
-    //ovoo div pr
+    //ovoo jee div prr
     const card= document.createElement("div");
 
-    //kk
+    //klasu smo dodLI
     card.classList.add("card");
-    card.textContent="?";
+    //NAS POCETNI ZNAAK
+    card.textContent="<3";
+    // cuvamo emoji
+    card.dataset.value = emoji;
+
+    // Klik na event
+    card.addEventListener("click", flipCard);
+
+    //dodavanje u boar
     gameBoard.appendChild(card);
 
 });
+
+function flipCard() {
+    //ako je lockBoard true nema klika
+    if (lockBoard){
+        return;
+    }
+    if (this === firstCard){
+        return;
+    }
+
+    //otvaranje kartice
+
+    this.textContent = this.dataset.value;
+    //ako nema prva kartica
+    if (!firstCard) {
+        firstCard = this;
+
+    } else {
+        secondCard= this;
+        checkMatch();
+    }
+}
+
+//Match logika, provera para
+function checkMatch(){
+    //uporedjuje dva elementa
+    const isMatch = firstCard.dataset.value === secondCard.dataset.value;
+
+    if (isMatch){
+        resetCards();
+    } else {
+        unflipCards();
+    }
+}
+
+//Zatvaranje kartica
+
+function unflipCards() {
+    lockBoard= true;
+    setTimeout(() => {
+
+    // Vraćanje ?
+    firstCard.textContent = "<3";
+    secondCard.textContent = "<3";
+
+    // Reset
+    resetCards();
+
+  }, 700);
+}
+
+function resetCards() {
+    firstCard= null;
+    secondCard =null;
+    lockBoard = false;
+}
+
+
+
+
 
 
 
