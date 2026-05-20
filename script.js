eb1d38a4554df57ed113b3e8621a31e414839aee
Praktikant Slot <praktikant.slot@v21.fazi.rs>
2026-05-20 13:30:23 +0200

    Izmene script fajla

--- script.js
+++ script.js
@@ -8,45 +8,26 @@ const cards = [
     "🪄",
     "🦄"];
 
-//dupliranjee spread operator
 const gameCard=[...cards, ...cards];
-//mesanje karata shuffle
 gameCard.sort (()=> Math.random() - 0.5);
 
-//uzimamoo board 
 const gameBoard = document.getElementById("gameBoard");
 const movesCounter = document.getElementById("moves");
 let firstCard = null;
 let secondCard = null; 
 let lockBoard = false;
-let moves=0;
+let moves = 0;
 
-
-//ovde kartice pravimo
 gameCard.forEach(emoji => {
-
-    //ovoo jee div prr
     const card= document.createElement("div");
-
-    //klasu smo dodali
     card.classList.add("card");
-
-    //NAS POCETNI ZNAAK
     card.textContent="<3";
-
-    // cuvamo emoji
     card.dataset.value = emoji;
-
-    // Klik na event
     card.addEventListener("click", flipCard);
-
-    //dodavanje u boar
     gameBoard.appendChild(card);
-
 });
 
 function flipCard() {
-    //ako je lockBoard true nema klika
 
     if (lockBoard){
         return;
@@ -55,38 +36,33 @@ function flipCard() {
         return;
     }
 
-    //otvaranje kartice
     this.textContent = this.dataset.value;
 
-    //ako nema prva kartica
     if (!firstCard) {
         firstCard = this;
 
     } else {
         secondCard= this;
+        moves++; 
+        movesCounter.textContent=moves;
         checkMatch();
     }
 }
-//Match logika, provera para
-function checkMatch(){
-    //uporedjuje dva elementa
-    const isMatch = firstCard.dataset.value === secondCard.dataset.value;
 
+function checkMatch(){
+    const isMatch = firstCard.dataset.value === secondCard.dataset.value;
     if (isMatch){
         resetCards();
     } else {
         unflipCards();
     }
 }
-//Zatvaranje kartica
+
 function unflipCards() {
     lockBoard= true;
     setTimeout(() => {
-
-    // Vraćanje <3
     firstCard.textContent = "<3";
     secondCard.textContent = "<3";
-    // Reset
     resetCards();
   }, 700);
 }
@@ -95,7 +71,6 @@ function resetCards() {
     firstCard= null;
     secondCard =null;
     lockBoard = false;
-    let moves=0;
 
 }
 
